a8919bcee0b6aff54f162b75aafeb7c623fa0fdc
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-03-18 13:38:41 +0100

    start tweaking experiment

--- experiment-simple.js
+++ experiment-simple.js
@@ -1,6 +1,6 @@
 
 /*
-
+ 
 terms:
 - environment
 - configuration
@@ -45,21 +45,24 @@ module.exports = {
           resolution: "hd",
           pixeldensity: 86,
           viewingdistance: 600,
-          gamma: 2.2
+          gamma: 2.2,
+          minintensity: 1/40
         },{
           id: "right",
           description: "Right external monitor",
           resolution: "hd",
           pixeldensity: 86,
           viewingdistance: 600,
-          gamma: 2.2
+          gamma: 2.2,
+          minintensity: 1/40
         },{
           id: "main",
           description: "Laptop internal monitor",
           resolution: "hd",
           pixeldensity: 157,
           viewingdistance: 500,
-          gamma: 2.2
+          gamma: 2.2,
+          minintensity: 1/40
       }],
       mouse: true,
       keyboard: true
@@ -123,8 +126,28 @@ module.exports = {
     destination: "./data",
     format: "json"
   }),
-  
+   
   experiments: [
+    snellen({
+      //rotate: random([-2,+2]), // add random rotation to prevent aliasing
+      pixelAlign: false,
+      lowIntensity: 0, //sequence.loop([0,0.25,0.5,0.75,0.9,0.95]),
+      //highIntensity: 1.0,
+      //contrastRatio: 1.05,
+      foregroundIntensity: 0,
+      backgroundIntensity: 1,
+      //size: "3px",
+      size: //sequence(["5mm","3mm","1mm"]), 
+      staircase({
+        startValue: "5mm",
+        stepSize: 1.2,
+        stepType: "multiply",
+        minReversals: 0, //5,
+        minTrials: 2
+        //minValue: 
+        //maxValue:
+      })
+    }),
     tao({
       vanishing: true,
       size: //sequence(["5mm","3mm","1mm"]), 
@@ -145,7 +168,6 @@ module.exports = {
       monitormessage: "Press Start or hit any key to start the experiment.",
       buttonlabel: "Start"
     }),*/
-    //bangbox(),
     /*
     centerline({
       centerLine: random.pick([true,false]),
@@ -161,30 +183,7 @@ module.exports = {
         //maxValue:
       })
     }),*/
-    snellen(
-    {
-      //rotate: random([-2,+2]), // add random rotation to prevent aliasing
-      pixelAlign: false,
-      lowIntensity: 0, //sequence.loop([0,0.25,0.5,0.75,0.9,0.95]),
-      //highIntensity: 1.0,
-      //contrastRatio: 1.05,
-      foregroundIntensity: 0,
-      backgroundIntensity: 1,
-      //size: "3px",
-      size: //sequence(["5mm","3mm","1mm"]), 
-      staircase({
-        startValue: "5mm",
-        stepSize: 1.2,
-        stepType: "multiply",
-        minReversals: 0, //5,
-        minTrials: 2
-        //minValue: 
-        //maxValue:
-      })
-    },
-    {
-      ambientIntensity: 0, //1/40,
-    }),
+    
     pause({
       buttondisplay: "control",
       displaymessage: "The experiment was completed successfully.\nThank you for your participation!",
