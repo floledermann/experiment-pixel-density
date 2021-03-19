5e3c2f342151e16e18d6698f48dc8bcf9262bc1a
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-03-19 09:32:14 +0100

    clean up experiment files to reflect current development

--- experiment.js
+++ experiment.js
@@ -1,70 +1,176 @@
 
-/*
+const snellen = require("stimsrv/src/tasks/snellen.js");
+const bangbox = require("stimsrv/src/tasks/bangbox.js");
+const tao = require("stimsrv/src/tasks/aucklandoptotypes.js");
+const pause = require("stimsrv/src/tasks/pause.js");
 
-terms:
-- environment
-- configuration
-- condition: level of an independent variable
-- stimulus: actual stimulus
-- experiment
-- trial
-- participant
+const filestorage = require("stimsrv/src/storage/filestorage.js");
 
-use cases:
-- mobile phone display
-- paper printout
-- Kindle PDF Output
-*/
+const staircase = require("stimsrv/src/controller/staircase.js");
+const random = require("stimsrv/src/controller/random.js");
+const sequence = require("stimsrv/src/controller/sequence.js");
 
-const lab = require("./setup-lab.js");
+const centerline = require("./src/tasks/centerline.js");
 
-const {sloan, landolt, auckland, vernier, random, staircase, pause} = require("stimsrv/src/index.js");
 
 // this is a complete configuration
 module.exports = {
   
-  name: "HR Display Experiment 1",
+  name: "Simple Test Experiment",
+    
+  devices: [
+    {
+      name: "Main PC",
+      id: "main",
+      ip: ".",
+      platform: "browser", // browser-old, browser-nojs, pdf, png
+      screens: [{
+          id: "left",
+          description: "Left external monitor",
+          resolution: "hd",
+          pixeldensity: 86,
+          viewingdistance: 600,
+          gamma: 2.2,
+          minintensity: 1/40
+        },{
+          id: "right",
+          description: "Right external monitor",
+          resolution: "hd",
+          pixeldensity: 86,
+          viewingdistance: 600,
+          gamma: 2.2,
+          minintensity: 1/40
+        },{
+          id: "main",
+          description: "Laptop internal monitor",
+          resolution: "hd",
+          pixeldensity: 157,
+          viewingdistance: 500,
+          gamma: 2.2,
+          minintensity: 1/40
+      }],
+      mouse: true,
+      keyboard: true
+    },
+    {
+      name: "Google Pixel 2",
+      id: "pixel2",
+      resolution: "hd",
+      pixeldensity: 440,
+      viewingdistance: 350,
+      gamma: 1.6,
+      touch: true
+    },
+    {
+      name: "Sony Xperia Z5-P",
+      id: "xperia",
+      resolution: "uhd",
+      pixeldensity: 801,
+      viewingdistance: 350,
+      gamma: 2.2,
+      touch: false  // viewing only
+    }
+  ],
   
-  devices: lab.devices,
+  roles: [
+    {
+      device: "main",
+      role: "supervisor",
+      interfaces: ["monitor", "control"],
+      description: "Supervisor screen and experiment control"
+    },
+    {
+      device: "main",
+      screen: "left",
+      role: "experiment",
+      interfaces: ["display","response"],
+      description: "Experiment screen for stimulus display and participant response"
+    },
+    {
+      device: "main",
+      screen: "left",
+      role: "experiment-debug",
+      interfaces: ["display","response","debug"],
+      description: "Experiment screen with debugging output"
+    },
+    {
+      device: "pixel2",
+      role: "experiment",
+      interfaces: ["display","response"],
+      description: "Experiment screen for stimulus display and participant response"
+    },
+    {
+      device: "xperia",
+      role: "experiment-display",
+      interfaces: ["display"],
+      description: "Experiment screen for stimulus display."
+    },
+  ],  
   
-  roles: lab.roles,
-  
-  // participant management etc.
-  
+  storage: filestorage({
+    destination: "./data",
+    format: "json"
+  }),
+   
   experiments: [
-    pause({
-      controller: "control",
-      controlmessage: "Press Start or hit any key to start the experiment.",
-      displaymessage: "Waiting for the experiment to start...",
-      buttonlabel: "Start"
+    snellen({
+      //rotate: random([-2,+2]), // add random rotation to prevent aliasing
+      pixelAlign: false,
+      lowIntensity: 0, //sequence.loop([0,0.25,0.5,0.75,0.9,0.95]),
+      //highIntensity: 1.0,
+      //contrastRatio: 1.05,
+      foregroundIntensity: 0,
+      backgroundIntensity: 1,
+      //size: "3px",
+      size: //sequence(["5mm","3mm","1mm"]), 
+      staircase({
+        startValue: "5mm",
+        stepSize: 1.2,
+        stepType: "multiply",
+        minReversals: 0, //5,
+        minTrials: 2
+        //minValue: 
+        //maxValue:
+      })
     }),
-    sloan({
-      size: ";10arcmin-2arcmin:*0.8",
-      letter: random(5),
-      exit: results => { if (!results) return true; }
+    centerline({
+      centerLine: random.pick([true,false]),
+      angle: random.range(0,360, {round: 1}),
+      foregroundIntensityHigh: false,
+      size: staircase({
+        startValue: "2mm",
+        stepSize: 1.2,
+        stepType: "multiply",
+        minReversals: 0, //5,
+        minTrials: 2
+        //minValue: 
+        //maxValue:
+      })
     }),
-    landolt({
-      size: ";10arcmin-2arcmin:*0.8",
-      orientation: random("0,90,180,270", 6)
-    }),
-    auckland({
-      duration: 500,
-      size: staircase.quest({})
-    }),
-    auckland({
-      duration: 500,
-      stroke: "none",
-      fill: "#000000",
-      size: staircase.quest({})
-    }),
-    auckland({
-      duration: 500,
+    tao({
       vanishing: true,
-      size: staircase.quest({})
+      size: staircase({
+        startValue: "5mm",
+        stepSize: 1.2,
+        stepType: "multiply",
+        minReversals: 0,
+        minTrials: 2
+        //minValue: 
+        //maxValue:
+      })
     }),
-    vernier({
-      size: 0,
-      gap: 0
+  /*
+    pause({
+      buttondisplay: "control",
+      displaymessage: "Waiting for the experiment to start...",
+      monitormessage: "Press Start or hit any key to start the experiment.",
+      buttonlabel: "Start"
+    }),*/   
+    pause({
+      buttondisplay: "control",
+      displaymessage: "The experiment was completed successfully.\nThank you for your participation!",
+      monitormessage: "Experiment ended.",
+      buttonlabel: "Restart"
     })
   ]
   
