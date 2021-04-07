51cae8a506442af1ee67cef7838ce4b5e591e6a4
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-04-07 11:58:11 +0200

    clean up loop example

--- experiment-loop.js
+++ experiment-loop.js
@@ -3,6 +3,7 @@ const pause = require("stimsrv/src/tasks/pause.js");
 const loop = require("stimsrv/src/tasks/loop.js");   
 
 const sequence = require("stimsrv/src/controller/sequence.js");
+const count = require("stimsrv/src/controller/count.js");
 
 const desktop = require("./config-desktop.js");
 
@@ -20,81 +21,54 @@ module.exports = {
   storage: desktop.storage,
   
   context: {
-    //pixeldensity: 96, this should be passed in from device info
-    message: sequence.loop(["Hi!", "Bye!"])
+    message: sequence.loop(["Odd number participant", "Even number participant"])
   },
   
   tasks: [
     pause({
       message: {
-        display: context => "Root\n" + context.message + "\n" + context.message2
+        display: context => "Task 1\n" + context.message + "\n" + (context.message2 || "")
       }
     }),
     loop({
-      initialContext: {
+      context: {
         displayDevice: sequence(["desktop","mobile-1","mobile-2"]),
-        loopCounter: 0 //count()
+        loopCounter: count()
       },
       loop: context => {
-        context.loopCounter++;
-        console.log("Checking outer loop counter " + context.loopCounter);
         return context.loopCounter < 2;
       },
       tasks: [
         pause({
+          store: true,  // this is the only task that gets stored (by default, store == false for the pause task)
           message: {
-            display: context => "2.1\n" + context.loopCounter + "\n" + context.message + "\n" + context.message2
+            display: context => "Task 2.1\nCounter: " + context.loopCounter + "\n" + context.message + "\n" + (context.message2 || "")
           }
-        }),/*
-        snellen({
-          foregroundIntensity: 0,
-          backgroundIntensity: 1,
-          size:
-            staircase({
-              startValue: "5mm",
-              stepSize: 1.2,
-              stepType: "multiply", 
-              minReversals: 0,
-              minTrials: 2
-            })
         }),
-        tao({
-          vanishing: true,
-          size: staircase({
-            startValue: "5mm",
-            stepSize: 1.2,
-            stepType: "multiply",
-            minReversals: 0,
-            minTrials: 2
-          })
-        }),*/
-        /*
         pause({
           message: {
-            display: context => "2.2\n" + context.loopCounter + "\n" + context.message + "\n" + context.message2
+            display: context => "Task 2.2\nCounter: " + context.loopCounter + "\n" + context.message + "\n" + (context.message2 || "")
           }
-        }),*/
+        }),
         loop({
-           initialContext: {
-            loopCounter: 5, //count()
-            message2: "Inner loop says hi!",
-            message: "Bye!"
+           context: {
+            loopCounter: 5,
+            message2: "Participant has seen Task 2.3!",
+            message: "Local message for Task 2.3"
           },
           loop: context => {
             context.loopCounter++;
-            console.log("Checking inner loop counter " + context.loopCounter);
             return context.loopCounter < 7;
           },
           tasks: [
             pause({
-              store: true,
               message: {
-                display: context => "2.3.1\n" + context.loopCounter + "\n" + context.message + "\n" + context.message2
+                display: context => "2.3.1\nCounter: " + context.loopCounter + "\n" + context.message + "\n" + context.message2
               }
             }),
             pause({
               message: {
-                display: context => "2.3.2\n" + context.loopCounter + "\n" + context.message + "\n" + context.message2
+                display: context => "2.3.2\nCounter: " + context.loopCounter + "\n" + context.message + "\n" + context.message2
               }
             })
           ],
@@ -107,7 +81,6 @@ module.exports = {
         monitor: "Experiment ended."
       },
       button: "Restart",
-      //buttondisplay: "control"
     }),
   ]
 }
