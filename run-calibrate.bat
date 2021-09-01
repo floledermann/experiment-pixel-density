aee8d2f8b548ce538074ad7b19337a5803c0415d
Florian Ledermann <florian.ledermann@tuwien.ac.at>
2021-09-01 10:31:59 +0200

    watch less aggressively

--- run-calibrate.bat
+++ run-calibrate.bat
@@ -1,4 +1,4 @@
 
-call npx nodemon --watch *.js --watch package.json --watch src --watch ../stimsrv/src --exec "npx" stimsrv calibrate.js
+call npx nodemon --watch calibrate.js --watch package.json --watch src --watch ../stimsrv/src --exec "npx" stimsrv calibrate.js
 
 pause
